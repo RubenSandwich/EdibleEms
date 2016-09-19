ad0a15997c89a2589f1e4acd42a794db63872723
Ruben NIculcea <ruben.niculcea@gmail.com>
2016-09-19 11:38:30 -0400

    Clean up masonry init code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
+var msnry = new Masonry('.grid', {
   // options
   itemSelector: '.grid-cell',
   columnWidth: 300,
-});
-
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  // options
 });
