fe5b9b7572c82f789cf1fe0c601cc49276bb9772
Ruben NIculcea <ruben.niculcea@gmail.com>
2016-09-26 16:37:38 -0400

    Load masonry initially and reload when images are done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 
+var msnry = new Masonry('.grid', {
+  // options
+  itemSelector: '.grid-cell',
+  isFitWidth: true,
+});
+
 imagesLoaded('.grid', function () {
-  var msnry = new Masonry('.grid', {
-    // options
-    itemSelector: '.grid-cell',
-    isFitWidth: true,
-  });
+  msnry.reloadItems();
 });
