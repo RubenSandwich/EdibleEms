94a9ae752ccd9c6e4b2140f1efe4e9443cdb0195
Ruben NIculcea <ruben.niculcea@gmail.com>
2016-09-27 11:06:14 -0400

    Force grid reload after each image load

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 
-var msnry = new Masonry('.grid', {
-  // options
+var elem = '.grid';
+
+var msnry = new Masonry(elem, {
   itemSelector: '.grid-cell',
   isFitWidth: true,
 });
 
-imagesLoaded('.grid', function () {
-  // Maybe reload after each inital image load?
+// Reload grid after each image load
+// The 'correct way' to do this is:
+// 1. Store image sizes in DOM
+// 2. Use image sizes to layout grid
+// This prevent reflows, but who has the time. :(
+imagesLoaded(elem).on('progress', function () {
   msnry.layout();
 });
