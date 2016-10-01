9345cd00b6d878fe77fb713248f6e68e700d50f9
Ruben NIculcea <ruben.niculcea@gmail.com>
2016-10-01 17:19:36 -0400

    Add hiding header

---
+++ js/edibleEm.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  var previousScrollTop = 0;
+  var header = document.getElementById('header');
+
+  // If scrolling down then hide the header
+  window.onscroll = function () {
+    // body.scrollTop is deprecated and no longer available on Firefox
+    var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (bodyScrollTop > previousScrollTop) {
+      header.classList.add('hide');
+    } else {
+      header.classList.remove('hide');
+    }
+
+    previousScrollTop = bodyScrollTop;
+  };
+
+  // If mouse movement close to the top of the screen then show the header
+  document.body.onmousemove = function (evt) {
+    if (evt.clientY <= header.clientHeight) {
+      header.classList.remove('hide');
+    }
+  };
+
+  // If mouse down or touch any of the header then show the whole header
+  header.onmousedown = function () {
+    header.classList.remove('hide');
+  };
+});
